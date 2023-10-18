8132101bd4a91f75735fba233785a6750ad75baa
MASDXI <sirawitt42@gmail.com>
2023-10-18 10:52:06 +0700

    fix: remove entrypoint and genesis

--- docker-compose/config/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d /geth-node/devnet/data/geth ]; then
-    echo "Initial genesis.json!"
-    geth --datadir /geth-node/devnet/data init /geth-node/devnet/config/genesis.json
-fi
-
-geth $@
